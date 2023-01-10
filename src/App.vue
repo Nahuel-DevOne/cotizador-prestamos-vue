7b2e5a5c47e55e4c1b6d561dd5c563fde6e770c2
Nahuel <nahuel.profesor@gmail.com>
2023-01-09 22:03:26 -0300

    Botones de incremento y decremento agregados

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,40 @@
     return formatter.format(cantidad.value);
   });
 
+  const handleChangeDecrement = () => {
+    const VALOR = cantidad.value - STEP
+    if( VALOR < MIN){
+      alert('Cantidad no válida');
+      return;
+    }
+    cantidad.value = VALOR
+  }
+
+  const handleChangeIncrement = () => {
+    const VALOR = cantidad.value + STEP
+    if( VALOR > MAX){
+      alert('Cantidad no válida');
+      return;
+    }
+    cantidad.value = VALOR
+  }
+
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div class="flex justify-between mt-10">
+      <button
+        class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+        @click="handleChangeDecrement"
+      >-</button>
+      <button
+        class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+        @click="handleChangeIncrement"
+      >+</button>
+    </div>
 
     <div class="my-5">
       <input 
